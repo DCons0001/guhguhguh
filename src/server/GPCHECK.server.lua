da2f1f4d3ef0adeeaf1820117722feade2b8a59c
Daniel <dsfxzfu@gmail.com>
2023-10-25 21:20:34 +0100

    Changed gp check

--- src/server/GPCHECK.server.lua
+++ src/server/GPCHECK.server.lua
@@ -1,21 +1,22 @@
---[[local MPS = game:GetService("MarketplaceService")
-local gcId = 0
-local scId = 0
+local mps = game:GetService("MarketplaceService")
+local gamepassId = 183548483 -- change this lol
 
 game.Players.PlayerAdded:Connect(function(player)
-    player.CharacterAdded:Connect(function(char)
-        if MPS:UserOwnsGamepassAsync(player.UserId, gcId) then
-            script.GravityCoil:Clone().Parent = player.Backpack
-        
-if MPS:UserOwnsGamepassAsync(player.UserId, scId) then
-    script.SpeedCoil:Clone().Parent = player.Backpack
-
-
-if player.MembershipType == Enum.MembershipType.Premium then
-    script.SuperCoil:Clone().Parent = player.Backpack
-                end
-            end
-        end
-    end)
+	player.CharacterAdded:Connect(function()
+		if mps:UserOwnsGamePassAsync(player.UserId,gamepassId) then
+			script.GravityCoil:Clone().Parent = player.Backpack
+		end
+	end)
 end)
---]]
+
+--
+
+local gamepassId = 183552044
+game.Players.PlayerAdded:Connect(function(player)
+	player.CharacterAdded:Connect(function()
+		if mps:UserOwnsGamePassAsync(player.UserId,gamepassId) then
+			script.SpeedCoil:Clone().Parent = player.Backpack
+		end
+	end)
+end)
+
